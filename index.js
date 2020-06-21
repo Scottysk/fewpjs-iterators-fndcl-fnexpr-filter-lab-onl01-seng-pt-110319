d665ff7228e02435ad3465e65467c99fbc08bac7
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-06-21 01:13:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ function findMatching(drivers, name) {
 function fuzzyMatch(drivers, name) {
   return drivers.filter(driver => driver[0].toUpperCase() === name[0].toUpperCase());
 }
+
+function matchName()
